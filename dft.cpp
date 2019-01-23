3fd613a2227b4959ce8f87ac1139c1622b92b8cc
Ashkan <Ashkan.Anousheh@gmail.com>
2019-01-23 07:40:31 +0330

    initialized

---
+++ dft.cpp
@@ -0,0 +1,89 @@
+#include <cmath>
+#include <vector>
+#include "matplotlibcpp.h"
+#include <complex>
+namespace plt = matplotlibcpp;
+
+class DFT
+{
+    using dft_t = std::vector<std::complex<float>>;
+    dft_t inp_, out_;
+    size_t N;
+
+public:
+    DFT( size_t N ) : N( N )
+    {
+        inp_.resize( N );
+        out_.resize( N );
+    }
+    void load( dft_t& data );
+    DFT& exec();
+    std::vector<float> mag();
+    std::vector<float> real();
+    std::vector<float> imag();
+    std::vector<float> phase();
+};
+void DFT::load( dft_t& data )
+{
+    // inp_.clear();
+    if ( inp_.size() >= data.size() )
+        {
+            for ( size_t i = 0; i < N; ++i )
+                {
+                    inp_[ i ].real( data[ i ].real() );
+                    inp_[ i ].imag( data[ i ].imag() );
+                }
+        }
+}
+DFT& DFT::exec()
+{
+    float real_part;
+    float imag_part;
+    out_.clear();
+    for ( size_t m = 0; m < N; m++ )
+        {
+            real_part = 0.0f;
+            imag_part = 0.0f;
+            for ( size_t n = 0; n < N; n++ )
+                {
+                    real_part += ( std::cos( 2.0f * M_PI * n * m / N )
+                                   * inp_[ n ].real() );
+                    imag_part += ( std::sin( 2.0f * M_PI * n * m / N )
+                                   * inp_[ n ].real() );
+                }
+            out_[ m ].real( real_part );
+            out_[ m ].imag( imag_part );
+        }
+    return *this;
+}
+std::vector<float> DFT::mag()
+{
+    std::vector<float> res( N );
+    for ( size_t i = 0; i < N; ++i )
+        {
+            res[ i ]
+                = std::sqrt( out_[ i ].real() * out_[ i ].real()
+                             + out_[ i ].imag() * out_[ i ].imag() );
+        }
+    return res;
+}
+int main()
+{
+    uint32_t fs, n, freq, N;
+    N    = 250;
+    freq = 1e3;
+    fs   = 10e3;
+    float angle;
+    std::vector<std::complex<float>> inp_signal( N );
+    for ( size_t n = 0; n < N; n++ )
+        {
+            angle = 2.0f * M_PI * freq * n / fs;
+            inp_signal[ n ].real( std::sin( angle ) );
+        }
+    DFT dft( N );
+    dft.load( inp_signal );
+    auto out = dft.exec().mag();
+    plt::plot( out );
+    plt::show();
+    return 0;
+}
